796f3fbd562a93e6186d4c8d58f28b2649d87055
SantiagoBobrik <santiago.bobrik@gmail.com>
2023-01-29 20:09:36 -0300

    initial commit

---
+++ organizer.py
@@ -0,0 +1,80 @@
+import time
+import os
+import getpass
+
+file_extensions = {
+    'images': ['jpg', 'jpeg', 'png', 'gif', 'svg', 'bmp', 'ico', 'psd', 'tif', 'tiff'],
+    'documents': ['doc', 'docx', 'pdf', 'txt', 'xls', 'xlsx', 'ppt', 'pptx'],
+    'apps': ['exe', 'dmg', 'pkg', 'deb', 'rpm'],
+    'compressed': ['zip', 'rar', 'tar', 'gz', '7z', 'iso'],
+    'sounds':  ['mp3', 'wav', 'ogg', 'flac', 'aac'],
+    'videos': ['mp4', 'mkv', 'avi', 'mov', 'wmv', 'flv', 'webm', '3gp'],
+    'other': ['bin', 'dat', 'db', 'log', 'msi', 'part', 'torrent', 'nupkg'],
+}
+
+
+def get_downlaod_dir():
+    # Download directory
+    WIN_DOWNLOAD_DIR = f"C:\\Users\\{getpass.getuser()}\\Downloads\\"
+    MAC_DOWNLOAD_DIR = f"/Users/{getpass.getuser()}/Downloads/"
+
+    # SO check
+    if os.name == 'nt':
+        download_dir = WIN_DOWNLOAD_DIR
+    elif os.name == 'MacOS':
+        download_dir = MAC_DOWNLOAD_DIR
+    else:
+        print("OS not supported.")
+        exit(1)
+    return download_dir
+
+
+def get_files(folder):
+    try:
+        files = os.listdir(folder)
+    except OSError:
+        print(f"Error getting files from {folder}:")
+        files = []
+    return files
+
+
+def main():
+
+    download_dir = get_downlaod_dir()
+    files = get_files(download_dir)
+    folders = file_extensions.keys()
+
+    for folder in folders:
+
+        for file in files:
+
+            file_extension = file.split('.')[-1]
+            folder_path = os.path.join(download_dir, folder)
+
+            if file_extension in file_extensions[folder]:
+
+                # Rename file if already exists. Format: timestamp-filename
+                renamed_file = None
+                if os.path.exists(os.path.join(folder_path, file)):
+                    renamed_file = f'{int(time.time())}-{file}'
+
+                # Move file to folder
+                file_path = os.path.join(download_dir, file)
+                dest_path = os.path.join(folder_path, renamed_file or file)
+
+                try:
+                    # Create folder if not exists
+                    if not os.path.exists(folder_path):
+                        os.mkdir(folder_path)
+
+                    os.rename(file_path, dest_path)
+
+                    print(f"Moved {file} to {folder}")
+                except OSError:
+                    print(f"Error moving {file} to {folder}")
+
+
+if __name__ == '__main__':
+    main()
+    print("Done!")
+    input('Press ENTER to exit')
